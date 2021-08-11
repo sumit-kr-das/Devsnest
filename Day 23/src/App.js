b5b8d54c3a16fef39f445922b20939595d0a3c91
Sumit Kumar Das <sumitkrdas777@gmail.com>
2021-08-11 21:08:11 +0530

    Add files via upload

---
+++ Day 23/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Error 404</h1>
+        <div className="coming">No THA Found</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
